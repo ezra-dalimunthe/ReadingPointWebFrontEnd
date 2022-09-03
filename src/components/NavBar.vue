e9370d3c637bb970076bf677cec9fc72b8a90739
Ezra Dalimunthe <ezra.dalimunthe@gmail.com>
2022-09-03 18:45:30 +0700

    fix navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,23 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="gradient-primary" fixed="top" class="mb-4">
-    <b-navbar-brand style="width:230px;" :to="{ name: 'home' }">Reading Point</b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-       
-      </b-navbar-nav>
-      <b-navbar-nav class="ml-auto" >
-       <b-nav-item :to="{ name: 'about' }">About</b-nav-item>
-        <b-nav-item-dropdown right v-if="false">
-          <template #button-content>
-            <em>{{ loggedInUserFullname }}</em>
-          </template>
-          <b-dropdown-item  :to="{ name: 'profile' }">Profile</b-dropdown-item>
-          <b-dropdown-item @click="logOut" href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
+  <b-navbar
+    toggleable="lg"
+    type="dark"
+    variant="gradient-primary"
+    fixed="top"
+    class="mb-4"
+  >
+    <b-navbar-brand :to="{ name: 'home' }">Reading Point</b-navbar-brand>
+    <button
+      type="button"
+      aria-label="Toggle navigation"
+      class="sidebar-toggler"
+      aria-expanded="false"
+      aria-controls="nav-collapse"
+      style="overflow-anchor: none"
+      @click="toggleSidebar"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
   </b-navbar>
 </template>
 <script>
@@ -37,6 +38,10 @@ export default {
           .then(this.$router.push({ name: "login" }));
       });
     },
+    toggleSidebar() {
+      var w = document.querySelector(".wrapper");
+      w.classList.toggle("nav-open");
+    },
   },
   computed: {
     loggedInUserFullname() {
@@ -46,3 +51,13 @@ export default {
   },
 };
 </script>
+<style>
+.sidebar-toggler {
+  padding: 0.25rem 0.75rem;
+  font-size: 1.25rem;
+  line-height: 1;
+  background-color: transparent;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+}
+</style>
