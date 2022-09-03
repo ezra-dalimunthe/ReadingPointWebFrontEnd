016a03f0173cfad47c19b66cf116f605464f05bf
Ezra Dalimunthe <ezra.dalimunthe@gmail.com>
2022-09-03 18:45:20 +0700

    fix detail part component

---
+++ src/components/DetailPart.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-row>
+    <b-col :cols="labelWidth">
+      {{ label }}
+    </b-col>
+    <b-col>:
+      {{ value }}
+    </b-col>
+  </b-row>
+</template>
+<script>
+export default {
+  name: "DetailPart",
+  props: {
+    label: {
+      type: String,
+      default: "-",
+    },
+    value: {
+      type: [String, Boolean, Number],
+      default: "-",
+    },
+    labelWidth:{
+        type:Number,
+        default:3
+    }
+  },
+};
+</script>
